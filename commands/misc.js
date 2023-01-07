60ff6a8d64658dd47852fede41fd28012d69a565
NIMA OFFICIAL <110774656+nimaofficial@users.noreply.github.com>
2023-01-07 13:36:52 +0530

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -3,10 +3,10 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : Dark Nero V-8
+ * @author : Kaveesha Sithum <https://github.com/Kaveeshasithum>
+ * @description : Dark Nero V-8,A Multi-functional whatsapp bot.
+ * @version 8
  **/
 
  const { tlang, getAdmin, prefix, Config, sck, fetchJson, runtime,cmd } = require('../lib')
