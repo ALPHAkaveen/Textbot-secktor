2b207a049649cc7e6805bc8c2df56a3bd60ae46a
NIMA OFFICIAL <110774656+nimaofficial@users.noreply.github.com>
2023-01-08 12:55:30 +0530

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -1,15 +1,3 @@
-
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Dark Nero V-8
- * @author : Kaveesha Sithum <https://github.com/Kaveeshasithum>
- * @description : Dark Nero V-8,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
-//---------------------------------------------------------------------------
 const { cmd }   = require('../lib');
 const util = require('util');
 const axios = require('axios');
